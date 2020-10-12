ac9b9cd14eea8afafc49b62d127cc53bf39d2665
Zieyaad King <k.m.zieyaaad@gmail.com>
2020-10-12 13:49:53 +0200

    start of project

---
+++ android/app/src/main/kotlin/com/example/coved19/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.coved19
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CovED19',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+    );
+  }
+}
