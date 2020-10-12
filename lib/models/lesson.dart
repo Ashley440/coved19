78a3bb3e646216adc7213636b9216d50f5bd05d7
Zieyaad King <k.m.zieyaaad@gmail.com>
2020-10-12 19:11:45 +0200

    Added functionality to info section

---
+++ lib/models/lesson.dart
@@ -0,0 +1,34 @@
+import 'package:expandable/expandable.dart';
+import 'package:flutter/material.dart';
+
+class Lesson {
+  final String question;
+  final String answer;
+
+  Lesson({this.question, this.answer});
+
+  Widget display() {
+    return Card(
+      child: ExpandablePanel(
+        header: Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 25),
+            child: Text(
+              question,
+              softWrap: true,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            )),
+        expanded: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            answer,
+            softWrap: true,
+            style: TextStyle(fontSize: 16, letterSpacing: 1.2, height: 1.5),
+            textAlign: TextAlign.justify,
+          ),
+        ),
+        // hasIcon: true,
+        // tapHeaderToExpand: true,
+      ),
+    );
+  }
+}
--- lib/views/info.dart
+++ lib/views/info.dart
@@ -1,4 +1,5 @@
 import 'package:coved19/constants/navigator.dart';
+import 'package:coved19/models/lesson.dart';
 import 'package:flutter/material.dart';
 
 class InfoPage extends StatefulWidget {
@@ -7,13 +8,37 @@ class InfoPage extends StatefulWidget {
 }
 
 class _InfoPageState extends State<InfoPage> {
+  List<Lesson> lessons = [
+    Lesson(
+        question: "What is covid19?",
+        answer:
+            "Coronavirus disease 2019 is an infectious disease caused by severe acute respiratory syndrome coronavirus 2"),
+    Lesson(
+        question: "Lorem ipsum dolor sit amet?",
+        answer:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+    Lesson(
+        question: "Sunt in culpa qui?",
+        answer:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+    Lesson(
+        question: "Consectetur adipiscing elit, sed do eiusmod?",
+        answer:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+    Lesson(
+        question: "Sed do?",
+        answer:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text("CovED19")),
       ),
-      body: Center(child: Text("This is the info page")),
+      body: ListView.builder(
+          itemCount: lessons.length,
+          itemBuilder: (context, index) => lessons[index].display()),
       bottomNavigationBar: NavigatorPane(),
     );
   }
