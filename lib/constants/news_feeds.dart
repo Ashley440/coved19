d7ad44e266e34a03c1a28fb828b2ab4b720dc6c4
Zieyaad King <k.m.zieyaaad@gmail.com>
2020-10-14 12:07:31 +0200

    Added webscraping to news section

--- lib/constants/news_feeds.dart
+++ lib/constants/news_feeds.dart
@@ -1,5 +1,8 @@
 import 'package:coved19/models/article.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' as parser;
+import 'package:html/dom.dart' as dom;
 
 class NewsFeeds extends StatefulWidget {
   @override
@@ -7,34 +10,36 @@ class NewsFeeds extends StatefulWidget {
 }
 
 class _NewsFeedsState extends State<NewsFeeds> {
-  List<Article> articles = [
-    Article(
-      headline:
-          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-      content: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "A iaculis at erat pellentesque adipiscing commodo elit at imperdiet. Amet purus gravida quis blandit. Quam pellentesque nec nam aliquam sem et tortor consequat. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. Imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada. Nisi quis eleifend quam adipiscing. Nulla aliquet porttitor lacus luctus. Consectetur adipiscing elit pellentesque habitant morbi tristique. Erat velit scelerisque in dictum non consectetur a erat nam. Porttitor leo a diam sollicitudin tempor id eu nisl nunc. Laoreet id donec ultrices tincidunt arcu non sodales neque sodales. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Nec ultrices dui sapien eget mi proin. Ut placerat orci nulla pellentesque dignissim enim sit. Accumsan sit amet nulla facilisi. Et tortor consequat id porta nibh venenatis."
-      ],
-      readmore: "",
-    ),
-    Article(
-      headline: "Elementum facilisis leo vel fringilla",
-      content: [
-        "A iaculis at erat pellentesque adipiscing commodo elit at imperdiet. Amet purus gravida quis blandit. Quam pellentesque nec nam aliquam sem et tortor consequat. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. Imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada. Nisi quis eleifend quam adipiscing. Nulla aliquet porttitor lacus luctus. Consectetur adipiscing elit pellentesque habitant morbi tristique. Erat velit scelerisque in dictum non consectetur a erat nam. Porttitor leo a diam sollicitudin tempor id eu nisl nunc. Laoreet id donec ultrices tincidunt arcu non sodales neque sodales. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Nec ultrices dui sapien eget mi proin. Ut placerat orci nulla pellentesque dignissim enim sit. Accumsan sit amet nulla facilisi. Et tortor consequat id porta nibh venenatis."
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      ],
-      readmore: "",
-    ),
-    Article(
-      headline:
-          "Tortor vitae purus faucibus ornare. Sollicitudin tempor id eu nisl",
-      content: [
-        "Ac turpis egestas sed tempus urna et pharetra pharetra massa. Gravida neque convallis a cras semper auctor neque vitae tempus. Lobortis feugiat vivamus at augue eget arcu dictum. Maecenas volutpat blandit aliquam etiam erat velit scelerisque in. Justo eget magna fermentum iaculis eu non diam phasellus. Arcu bibendum at varius vel pharetra vel turpis nunc. Tortor posuere ac ut consequat semper. Ac auctor augue mauris augue. Tortor vitae purus faucibus ornare. Sollicitudin tempor id eu nisl. Sed odio morbi quis commodo odio aenean sed. Nunc sed id semper risus in hendrerit gravida rutrum quisque. Neque ornare aenean euismod elementum nisi quis eleifend quam. Nibh cras pulvinar mattis nunc sed blandit libero. Sit amet facilisis magna etiam tempor. Eu consequat ac felis donec et. Risus in hendrerit gravida rutrum. Fermentum posuere urna nec tincidunt. Fames ac turpis egestas maecenas pharetra convallis posuere morbi.",
-        "Maecenas pharetra convallis posuere morbi leo urna molestie at elementum. Condimentum id venenatis a condimentum. Nam at lectus urna duis convallis convallis tellus id. Cursus euismod quis viverra nibh. Malesuada fames ac turpis egestas integer eget aliquet. Odio pellentesque diam volutpat commodo. Vel facilisis volutpat est velit. Facilisi cras fermentum odio eu. Tortor vitae purus faucibus ornare. Risus in hendrerit gravida rutrum quisque non tellus. Aliquam sem et tortor consequat id porta nibh venenatis cras. Cursus eget nunc scelerisque viverra mauris. Amet massa vitae tortor condimentum lacinia quis."
-      ],
-      readmore: "",
-    ),
-  ];
+  List<Article> articles = [];
+
+  Future<void> getArticles() async {
+    String link = "https://sacoronavirus.co.za/category/in-the-media/";
+    var response = await http.get(link);
+    dom.Document newsWebPage = parser.parse(response.body);
+    for (int i = 0; i < 5; i++) {
+      var articleBlock = newsWebPage.getElementsByTagName("article")[i];
+      var articleImage =
+          articleBlock.getElementsByTagName("img")[0].attributes["src"];
+      var articleLink = articleBlock.getElementsByTagName("a")[0];
+      Article article = Article(
+          headline: articleLink.text,
+          imageLink: articleImage,
+          readmore: articleLink.attributes["href"]);
+      await article.getArticleContent();
+      articles.add(article);
+    }
+    // Updating the articles on page
+    setState(() {
+      articles = articles;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    getArticles();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/models/article.dart
+++ lib/models/article.dart
@@ -1,13 +1,30 @@
 import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:html/dom.dart' as dom;
+import 'package:html/parser.dart' as parser;
 
 class Article {
   final String headline;
-  // final String imageLink;
-  final List<String> content;
+  final String imageLink;
+  List<String> _content;
   final String readmore;
 
-  Article({this.headline, this.content, this.readmore});
+  Article({this.headline, this.imageLink, this.readmore});
+
+  Future<void> getArticleContent() async {
+    var response = await http.get(readmore);
+    dom.Document articleContent = parser.parse(response.body);
+    var paragraphs = articleContent
+        .getElementsByClassName('post-content')[0]
+        .getElementsByTagName("p");
+    // Setting content
+    _content = [];
+    for (int i = 0; i < paragraphs.length - 2; i++) {
+      _content.add(paragraphs[i].innerHtml);
+    }
+    // print(_content);
+  }
 
   Widget display() {
     return Card(
@@ -16,14 +33,15 @@ class Article {
               padding: EdgeInsets.symmetric(horizontal: 10, vertical: 25),
               child: Row(
                 children: [
-                  // Image.network(imageLink),
                   Expanded(
-                    child: Container(
-                      alignment: Alignment.center,
-                      // width: 50,
-                      height: 80,
-                      color: Colors.red,
-                    ),
+                    child: imageLink != null
+                        ? Image.network(imageLink)
+                        : Container(
+                            alignment: Alignment.center,
+                            // width: 50,
+                            height: 80,
+                            color: Colors.red,
+                          ),
                   ),
                   SizedBox(
                     width: 10,
@@ -43,15 +61,20 @@ class Article {
                 padding: EdgeInsets.all(16.0),
                 child: Column(
                   children: [
-                    Text(
-                      content.join("\n\n"),
-                      softWrap: true,
-                      style: TextStyle(
-                          fontSize: 16, letterSpacing: 1.2, height: 1.5),
-                      textAlign: TextAlign.justify,
-                    ),
-                    SizedBox(height: 25),
-                    // Insert link widget that takes you directly to page
+                    _content == null
+                        ? Center(
+                            child: Text("loading article...",
+                                style: TextStyle(fontStyle: FontStyle.italic)),
+                          )
+                        : Text(
+                            _content.join("\n\n"),
+                            softWrap: true,
+                            style: TextStyle(
+                                fontSize: 16, letterSpacing: 1.2, height: 1.5),
+                            textAlign: TextAlign.justify,
+                          ),
+                    // SizedBox(height: 25),
+                    // // Insert link widget that takes you directly to page
                   ],
                 ))));
   }
